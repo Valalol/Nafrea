bc75048c2fb2d9bc804349d4cd8e92b6873bebe5
Valentin ESNAULT <E218577J@etu.polytech.univ-nantes.prive>
2023-01-25 16:21:04 +0100

    Water_bar (health bar) added and some improvements to the divs

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+const menu = document.getElementById("side_menu");
+const game = document.getElementById("game_window")
+const timeline = document.getElementById('water_timeline');
+const water_bar = document.getElementById("water_bar_colored")
+
+
 function shrink_menu() {
-    menu = document.getElementById("side_menu");
-    game = document.getElementById("game_window")
     if (menu.style.left == "-25%") {
         menu.style.left = "0";
-        game.style.width = "75%"
-        game.style.marginLeft = "25%"
+        game.style.width = "75%";
+        game.style.marginLeft = "25%";
     } else {
         menu.style.left = "-25%";
-        game.style.width = "100%"
-        game.style.marginLeft = "0"
+        game.style.width = "100%";
+        game.style.marginLeft = "0";
     }
 }
 
-const timeline = document.getElementById('water_timeline');
+function new_frame() {
+    new_value = 0.5;
+
+    date_fin = date_fin + 1;
+    water_data.push(new_value);
+
+    water_bar.style.height = new_value*100+"%";
+    console.log("bah là normalement y'a des trucs mis à jour")
+}
+
+
+
 
 const date_debut = 2023;
 var date_fin = 2033;
